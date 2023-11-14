c02062ae1f5bb0a87c483508a5a1f928a3345963
atabar19 <carlkapz@gmail.com>
2023-11-14 15:03:50 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -1,10 +1,46 @@
 from settings import *
-from mongo_config import weather_data_collection
-from mongo_config import pollution_data_collection
+from mongo_config import weather_data_collection, pollution_data_collection
+from timeloop import Timeloop
+from datetime import timedelta
+import requests
+import json
 
-def insert_to_mongo():
-  print("inserting into mongo")
-  weather_data_collection.insert_one({'weather2':'cold'})
-  print("done inserting!!!")
+tl = Timeloop()
 
-insert_to_mongo()
+def get_weather_data():
+	""" get weather data from climacell """
+	url = "https://api.tomorrow.io/v4/weather/realtime?location=beijing&units=metric&apikey=67t7rBMPnWRiBvP4raM1091eUdSNtDDJ"
+	headers = {"accept": "application/json"}
+	response = requests.get(url, headers=headers)
+	return response.json()
+	#url = "https://api.climacell.co/v3/weather/realtime"
+	#querystring = {"lat":"39.9042","lon":"116.4074","unit_system":"us","fields":"precipitation,wind_gust,humidity,wind_direction,precipitation_type,visibility,cloud_cover,cloud_base,cloud_ceiling,weather_code,feels_like,temp","apikey":CLIMACELL_API_KEY}
+	#weather_response = requests.request("GET", url, params=querystring)
+	#return weather_response.json()
+
+# def get_air_pollution_data():
+# 	""" get air quality data from climacell """
+# 	url = "https://api.climacell.co/v3/weather/realtime"
+# 	#querystring = {"lat":"39.9042","lon":"116.4074","unit_system":"us","fields":"o3,so2,co,no2,pm10,pm25","apikey":CLIMACELL_API_KEY}
+# 	air_pollution_response = requests.request("GET", url, params=querystring)
+# 	return air_pollution_response.json()
+
+@tl.job(interval=timedelta(seconds=5))
+def sample_job_every_120s():
+	weather_response = get_weather_data()
+	#air_pollution_data = get_air_pollution_data()
+	insert_to_mongo(weather_response)
+	#insert_to_mongo(weather_response, air_pollution_data))
+
+def insert_to_mongo(weather_data):
+#def insert_to_mongo(weather_data, air_pollution_data):
+	""" insert weather data and traffic data to the proper collections """
+	weather_data_collection.insert_one(weather_data)
+	#pollution_data_collection.insert_one(air_pollution_data)
+
+def main():
+	sample_job_every_120s()
+
+if __name__ == "__main__":
+	tl.start(block=True)
+	main()
--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ load_dotenv()
 
 #MONGO_URI = os.environ.get('MONGO_URI')
 MONGO_URI = 'mongodb+srv://carl-admin:carl_22012331@cluster0.zvcu3a1.mongodb.net/?retryWrites=true&w=majority'
+#OPENWEATHERMAP_API_KEY='d01b6e340be9dcfdf743601a5723cbe8'
+CLIMACELL_API_KEY='67t7rBMPnWRiBvP4raM1091eUdSNtDDJ'
+#ROCKSET_API_KEY='your api key'
